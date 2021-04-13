d29ffedc741738caa63a4ccfbdfee9657836ed15
lukebergmann <lukebergmann0@gmail.com>
2021-04-12 22:48:05 -0600

    Created sidebar using storybook to create smaller peices.

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,19 @@
 import React from "react";
+import classNames from "classnames"
 
 import "components/Button.scss";
 
 export default function Button(props) {
-   return <></>;
+   const buttonClass = classNames('button', {"button--confirm": props.confirm, "button--danger": props.danger})
+
+   // if (props.confirm) {
+   //    buttonClass += " button--confirm";
+   // }
+   // if (props.danger) {
+   //    buttonClass += " button--danger";
+   // }
+
+
+   return <button disabled={props.disabled} onClick={props.onClick} className={buttonClass}>{props.children}</button>;
+
 }
---
+++ src/components/DayListItem.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import classNames from "classnames"
+import "components/DayListItem.scss";
+
+export default function DayListItem(props) {
+  const dayClass = classNames('day-list__item', { "day-list__item--selected": props.selected, "day-list__item--full": props.spots === 0 })
+
+  const formatSpots = function (numOfSpots) {
+    if (!numOfSpots) {
+      return 'no spots remaining'
+    } else if (numOfSpots === 1) {
+      return '1 spot remaining'
+    } else {
+      return `${numOfSpots} spots remaining`
+    }
+  }
+
+  return (
+    <li onClick={() => props.setDay(props.name)}>
+      <div className={dayClass}>
+        <h2 className="text--regular">{props.name}</h2>
+        <h3 className="text--light">{formatSpots(props.spots)}</h3>
+      </div>
+    </li>
+  );
+
+}
