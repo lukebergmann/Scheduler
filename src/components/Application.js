72a12298dab90986e6156e6f9c274b151e3165a8
lukebergmann <lukebergmann0@gmail.com>
2021-04-14 18:23:46 -0600

    created helper function that returns appointments for a given day

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,10 +1,13 @@
 // LIBRARY
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios"
 // COMPONENTS
 import DayList from "components/DayList";
 import Appointment from "components/Appointment"
 // STYL
 import "components/Application.scss";
+
+
 
 const appointments = [
   {
@@ -54,33 +57,42 @@ const appointments = [
 ];
 
 
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
+// const days = [
+//   {
+//     id: 1,
+//     name: "Monday",
+//     spots: 2,
+//   },
+//   {
+//     id: 2,
+//     name: "Tuesday",
+//     spots: 5,
+//   },
+//   {
+//     id: 3,
+//     name: "Wednesday",
+//     spots: 0,
+//   },
+// ];
 
 
 export default function Application(props) {
   const [day, setDay] = useState("Monday");
+  const [days, setDays] = useState([]);
   const aptList = appointments.map((appointment) => {
     return (
       <Appointment
         key={appointment.id} {...appointment}
       />)
   });
+
+  useEffect(() => {
+    axios.get(`/api/days`)
+    .then(response => {
+      setDays(response.data) 
+    })
+
+  }, [])
 
   return (
     <main className="layout">
---
+++ src/helpers/selectors.js
@@ -0,0 +1,17 @@
+export function getAppointmentsForDay(state, selectedDay) {
+  //... returns an array of appointments for that day
+  const specificDay = []
+  for (let day of state.days) {
+    if (day.name === selectedDay) {
+      for (let appointment in state.appointments) {
+        if (day.appointments.includes(Number(appointment))) {
+          specificDay.push(state.appointments[appointment]);
+        }  
+      }
+    }
+  }
+
+
+
+  return specificDay;
+};
